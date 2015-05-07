bd7a847ae78e9b90c9db9740be89f8afdde7b7d1
Colin B. Macdonald <macdonald@maths.ox.ac.uk>
2015-05-07 14:16:39 +0100

    doctest_compare: rework test for matlab/octave compat
    Fixes #32.

--- inst/private/doctest_compare.m
+++ inst/private/doctest_compare.m
@@ -10,13 +10,18 @@ function match = doctest_compare(want, got)
 %
 %
 % They also sometimes backspace over things for no apparent reason.  This
-% doctest recreates that condition.
+% doctest recreates that condition.  Note a bit of fuss here because Octave
+% needs this escape sequence in double quotes which Matlab won't parse.
 %
-% >> sprintf('Hi there, there is no dot here: .\x08')
-% 
+% >> if (exist('OCTAVE_VERSION'))
+% ..   eval('sprintf("Hi, no question mark here?\x08 goodbye")')
+% .. else
+% ..   sprintf('Hi, no question mark here?\x08 goodbye')
+% .. end
+%
 % ans =
-% 
-% Hi there, there is no dot here: 
+%
+% Hi, no question mark here goodbye
 %
 %
 % All of the doctests should pass, and they manipulate this function.
