a43d0641affa5a2f815b7113147e624174d65d79
Michael Walter <michael.walter@gmail.com>
2015-05-13 17:40:07 +0200

    Fixed typo in test_angle_brackets doctest.

--- test/test_angle_brackets.m
+++ test/test_angle_brackets.m
@@ -1,7 +1,7 @@
 function s = test_angle_brackets()
 % https://savannah.gnu.org/bugs/?45084 (Fixed in Octave 4.0)
 %
-% >> s = angle_brackets()
+% >> s = test_angle_brackets()
 % s = I <3 U
 % >> s = '<p>I heart you</p>'
 % s = <p>I heart you</p>
