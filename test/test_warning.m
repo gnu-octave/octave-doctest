0ba76cb9860a5bb091685ebce340dee40439115d
Michael Walter <michael.walter@gmail.com>
2015-05-18 03:27:36 +0200

    Fix warning with older Octaves (?).

--- test/test_warning.m
+++ test/test_warning.m
@@ -1,6 +1,6 @@
 function test_warning()
 % >> toeplitz ([1 2], [2 3])
-% ***arning: ***olumn wins diagonal conflict***
+% ***arning: ***olumn wins ***diagonal conflict***
 % ans =
 %    1   3
 %    2   1
