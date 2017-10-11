b70bb5ea2a4160b7728c75699eb35501b6c250eb
Colin B. Macdonald <cbm@m.fsf.org>
2017-10-11 16:12:35 -0700

    Store data in a subfunction to allow "clear" in a test
    Also, use mlock and persistent which seems to support "clear all" too.

--- inst/private/doctest_run_tests.m
+++ inst/private/doctest_run_tests.m
@@ -22,13 +22,16 @@ if is_octave()
   split_long_rows(0, 'local')
 end
 
+DOCTEST__datastore('set_all', DOCTEST__tests)
+
 % define test-global constants (these are accessible by the tests)
 DOCTEST_OCTAVE = is_octave();
 DOCTEST_MATLAB = ~DOCTEST_OCTAVE;
 
-DOCTEST__results = [];
 for DOCTEST__i = 1:numel(DOCTEST__tests)
-  DOCTEST__result = DOCTEST__tests(DOCTEST__i);
+  %DOCTEST__result = DOCTEST__tests(DOCTEST__i);
+  DOCTEST__datastore('init_i', DOCTEST__i)
+  DOCTEST__result = DOCTEST__datastore('get_ith');
 
   % determine whether test should be skipped
   % (careful about Octave bug #46397 to not change the current value of “ans”)
@@ -36,6 +39,7 @@ for DOCTEST__i = 1:numel(DOCTEST__tests)
                  DOCTEST__join_conditions (DOCTEST__result.skip), ...
                 ';'));
   if (DOCTEST__result.skip)
+     DOCTEST__datastore('set_ith', DOCTEST__result);
      continue
   end
 
@@ -44,13 +48,16 @@ for DOCTEST__i = 1:numel(DOCTEST__tests)
   eval (strcat ('DOCTEST__result.xfail = ', ...
                  DOCTEST__join_conditions (DOCTEST__result.xfail), ...
                 ';'));
+  DOCTEST__datastore('set_ith', DOCTEST__result);
 
-  % evaluate input (structure adapted from a StackOverflow answer by user Amro, see http://stackoverflow.com/questions/3283586 and http://stackoverflow.com/users/97160/amro)
   try
-    DOCTEST__result.got = evalc(DOCTEST__result.source);
+    DOCTEST__got = evalc(DOCTEST__result.source);
   catch DOCTEST__exception
-    DOCTEST__result.got = DOCTEST__format_exception(DOCTEST__exception);
+    DOCTEST__got = DOCTEST__format_exception(DOCTEST__exception);
   end
+  % pull from datastore (in case test did "clear")
+  DOCTEST__result = DOCTEST__datastore('get_ith');
+  DOCTEST__result.got = DOCTEST__got;
 
   % determine if test has passed
   DOCTEST__result.passed = doctest_compare(DOCTEST__result.want, DOCTEST__result.got, DOCTEST__result.normalize_whitespace, DOCTEST__result.ellipsis);
@@ -58,13 +65,54 @@ for DOCTEST__i = 1:numel(DOCTEST__tests)
     DOCTEST__result.passed = ~DOCTEST__result.passed;
   end
 
-  DOCTEST__results = [DOCTEST__results; DOCTEST__result];
+  %DOCTEST__results = [DOCTEST__results; DOCTEST__result];
+  DOCTEST__datastore('set_ith', DOCTEST__result);
 end
 
+DOCTEST__results = DOCTEST__datastore('get_all');
+DOCTEST__datastore('clear');
+end
+
+
+function out = DOCTEST__datastore(action, var)
+  % store variables in a way that survives "clear" and "clear all".
+
+  % try to survive a test doing "clear" and "clear all"
+  % https://github.com/catch22/octave-doctest/issues/149
+  mlock()
+  persistent i results
+
+  switch lower(action)
+    case 'clear'
+      % don't leave mlocked persistent data lying around
+      results = [];
+      i = [];
+    case 'set_all'
+      % cell array so it can be heterogeneous
+      results = num2cell(var);
+    case 'init_i'
+      i = var;
+    case 'set_ith'
+      results{i} = var;
+    case 'get_ith'
+      out = results{i};
+    case 'get_all'
+      % unwrap from cell-array, discarding skips
+      %out = cell2mat(results);  % fails b/c they have different fields
+      out = [];
+      for j=1:numel(results)
+        if (~ any(results{j}.skip))
+          out = [out results{j}];
+        end
+      end
+    otherwise
+      error ('unexpected action')
+  end
 end
 
 
 function formatted = DOCTEST__format_exception(ex)
+  mlock()  % not clear why we need this
 
   if is_octave()
     formatted = ['??? ' ex.message];
@@ -85,6 +133,7 @@ end
 % given a cell array of conditions (represented as strings to be eval'ed),
 % return the string that corresponds to their logical "or".
 function result = DOCTEST__join_conditions(conditions)
+  mlock()  % not clear why we need this
   if isempty(conditions)
     result = 'false';
   else
