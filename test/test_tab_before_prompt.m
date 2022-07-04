62e6e92235e369fbd00c9ba76a195288910c82cb
Colin B. Macdonald <cbm@m.fsf.org>
2022-07-04 00:21:35 -0700

    move copyright messages to fix tests on Octave 6

--- test/test_tab_before_prompt.m
+++ test/test_tab_before_prompt.m
@@ -1,8 +1,4 @@
 function test_tab_before_prompt()
-%
-% Copyright (c) 2019 Colin B. Macdonald
-% SPDX-License-Identifier: BSD-3-Clause
-%
 % This first test has spaces
 %       >> a = 42
 %       a = 42
@@ -12,3 +8,6 @@ function test_tab_before_prompt()
 %
 %
 % The second one has tabs: there should be two tests.
+%
+% Copyright (c) 2019, 2022 Colin B. Macdonald
+% SPDX-License-Identifier: BSD-3-Clause
