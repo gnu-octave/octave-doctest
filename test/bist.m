6e5b775eeb4caf3b2d1632fb1d8398d3b4537550
Colin B. Macdonald <cbm@m.fsf.org>
2025-09-13 00:24:32 -0700

    only test classdef delete stuff on OCtave >= 9

--- test/bist.m
+++ test/bist.m
@@ -286,14 +286,18 @@ end
 
 %!test
 %! % classdef handle subclass delete behaviour
-%! [n, t, summary] = doctest ('cdef_subhandle1');
-%! assert (n == t)
-%! assert (n == 4)
-%! assert (summary.num_targets_with_extraction_errors == 0)
+%! if (compare_versions (OCTAVE_VERSION(), '9.0.0', '>='))
+%!   [n, t, summary] = doctest ('cdef_subhandle1');
+%!   assert (n == t)
+%!   assert (n == 4)
+%!   assert (summary.num_targets_with_extraction_errors == 0)
+%! end
 
 %!test
 %! % classdef handle subclass delete behaviour
-%! [n, t, summary] = doctest ('cdef_subhandle2');
-%! assert (n == t)
-%! assert (n == 3)
-%! assert (summary.num_targets_with_extraction_errors == 0)
+%! if (compare_versions (OCTAVE_VERSION(), '9.0.0', '>='))
+%!   [n, t, summary] = doctest ('cdef_subhandle2');
+%!   assert (n == t)
+%!   assert (n == 3)
+%!   assert (summary.num_targets_with_extraction_errors == 0)
+%! end
